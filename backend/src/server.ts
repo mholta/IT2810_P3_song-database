e1bd373e6f5ea290fa5adff8283d097c8b38af3a
Håkon Nese <haaknes@stud.ntnu.no>
2021-10-11 12:27:43 +0200

    Finish setup with connection to db #2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,28 @@
 
-import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServer } from "apollo-server-express";
 import express from "express";
-const app = express();
-const typeDefs = gql`
-    type Query {
-        hello: String!
-    }
-`;
-const resolvers = {
-    Query: {
-        hello: () => "hei"
-    }
-};
-const server = new ApolloServer({typeDefs, resolvers});
+import mongoose from "mongoose";
+import {resolvers} from "./models/resolvers";
+import { typeDefs } from "./models/typeDefs";
+require('dotenv').config();
 
-server.applyMiddleware( {app});
+const startServer = async () => {
+    const app = express();
 
-app.listen({ port:4000},()=> {
-    console.log(`http://localhost:4000${server.graphqlPath}`)
-})
+
+    const server = new ApolloServer({typeDefs, resolvers});
+    
+    server.applyMiddleware( {app});
+    await mongoose.connect(`mongodb://${process.env.DB_IP}:${process.env.DB_PORT}`, {
+        user: process.env.DB_USERNAME,
+        pass: process.env.DB_PASSWORD,
+        dbName: process.env.DB_NAME
+    });
+    
+    app.listen({ port:4000},()=> {
+        console.log(`http://localhost:4000${server.graphqlPath}`)
+    })
+}
+
+startServer();
+
