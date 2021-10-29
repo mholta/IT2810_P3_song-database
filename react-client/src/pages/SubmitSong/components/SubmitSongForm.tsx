4a3e489a0c9322f0da0cfc1d776e8e48e69b18d0
Håkon Nese <haaknes@stud.ntnu.no>
2021-10-29 23:03:12 +0200

    Route to newly created song #32

--- react-client/src/pages/SubmitSong/components/SubmitSongForm.tsx
+++ react-client/src/pages/SubmitSong/components/SubmitSongForm.tsx
@@ -1,10 +1,18 @@
 import { useMutation, gql } from '@apollo/client';
-import { Autocomplete, Button, Stack, TextField } from '@mui/material';
+import {
+  Autocomplete,
+  Button,
+  CircularProgress,
+  Stack,
+  TextField,
+} from '@mui/material';
 import { styled } from '@mui/system';
 import React, { useReducer, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 import { RootState } from '../../../store';
 import { albumReducer, initialAlbumState } from '../album/album.reducer';
+import { RouteFolders } from '../../MainRouter';
 import {
   setAlbumId,
   setAppleMusicLink,
@@ -47,7 +55,10 @@ const SubmitSongForm = ({}: SubmitSongFormProps) => {
   ] = useState<boolean>(false);
 
   // Mutation
-  const [createSong, { loading, error }] = useMutation(CREATE_SONG_MUTATION);
+  const [createSong, { data, loading, error }] = useMutation(
+    CREATE_SONG_MUTATION
+  );
+  const history = useHistory();
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
@@ -86,202 +97,207 @@ const SubmitSongForm = ({}: SubmitSongFormProps) => {
       },
     });
   };
-
-  if (loading) return <>'Submitting...'</>;
+  if (data) {
+    console.log(data && data?.createSong && data?.createSong?._id);
+    console.log(RouteFolders.SONG + '/' + data.createSong._id);
+    history.replace(RouteFolders.SONG + '/' + data.createSong._id);
+  }
+  // if (loading) return <>'Submitting...'</>;
   if (error) return <>`Submission error! ${error.message}`</>;
 
   return (
-    <>
-      <SubmitSongFormWrapper
-        action="/"
-        onSubmit={handleSubmit}
-        onChange={() => setInputError('')}
-        onKeyPress={(e: any) => {
-          // Prevent for submission when clicking enter, but allow
-          // default behaviour on textareas
-          if (e.key === 'Enter' && e.target?.nodeName !== 'TEXTAREA')
-            e.preventDefault();
-        }}
-      >
-        {/* Submitter artist */}
-        <ArtistSearch
-          setValueCallback={(value: string) => dispatch(setMainArtist(value))}
-        />
+    <SubmitSongFormWrapper
+      action="/"
+      onSubmit={handleSubmit}
+      onChange={() => setInputError('')}
+      onKeyPress={(e: any) => {
+        // Prevent for submission when clicking enter, but allow
+        // default behaviour on textareas
+        if (e.key === 'Enter' && e.target?.nodeName !== 'TEXTAREA')
+          e.preventDefault();
+      }}
+    >
+      {/* Submitter artist */}
+      <ArtistSearch
+        setValueCallback={(value: string) => dispatch(setMainArtist(value))}
+      />
 
-        {/* Album */}
-        {state.mainArtistId &&
-          (createNewAlbumModalOpen ? (
-            <ModalBox>
-              <CreateNewAlbum
-                state={albumState}
-                dispatch={albumDispatch}
-                setCreateNewAlbumModalOpen={setCreateNewAlbumModalOpen}
-              />
-            </ModalBox>
-          ) : (
-            <AlbumSelect
-              artistId={state.mainArtistId}
-              setValueCallback={(value: string) => dispatch(setAlbumId(value))}
-              setNewAlbumModalOpenCallback={() =>
-                setCreateNewAlbumModalOpen(true)
+      {/* Album */}
+      {state.mainArtistId &&
+        (createNewAlbumModalOpen ? (
+          <ModalBox>
+            <CreateNewAlbum
+              state={albumState}
+              dispatch={albumDispatch}
+              setCreateNewAlbumModalOpen={setCreateNewAlbumModalOpen}
+            />
+          </ModalBox>
+        ) : (
+          <AlbumSelect
+            artistId={state.mainArtistId}
+            setValueCallback={(value: string) => dispatch(setAlbumId(value))}
+            setNewAlbumModalOpenCallback={() =>
+              setCreateNewAlbumModalOpen(true)
+            }
+          />
+        ))}
+
+      {/* Title */}
+      <TextField
+        required
+        label="Tittel"
+        id="song-title"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          dispatch(setTitle(e.target.value))
+        }
+        value={state.title}
+      />
+
+      {/* Release date */}
+      <DatePicker
+        label="Utgivelsesdato"
+        value={state.releaseDate}
+        onChange={(newValue) => dispatch(setReleaseDate(newValue))}
+        renderInput={(params) => <TextField {...params} />}
+      />
+
+      {/* Themes */}
+      <Autocomplete
+        multiple
+        id="themes"
+        options={allThemes}
+        getOptionLabel={(theme) => theme.title}
+        value={state.themes}
+        onChange={(_, newValue) => {
+          dispatch(setThemes(newValue));
+        }}
+        noOptionsText="Kunne ikke hente tema."
+        renderInput={(params) => {
+          const themesLimit = 4;
+          const themesCount = state.themes.length;
+          const tooManyChosen = themesCount > themesLimit;
+
+          return (
+            <TextField
+              {...params}
+              variant="outlined"
+              label="Tema"
+              placeholder={themesCount + 1 > themesLimit ? '' : 'Tema'}
+              error={tooManyChosen}
+              helperText={
+                tooManyChosen
+                  ? 'For mange tema. Du kan maks 4 tema.'
+                  : 'Velg opp til 4 tema.'
               }
             />
-          ))}
+          );
+        }}
+      />
 
-        {/* Title */}
+      {/* Key, tempo, time */}
+      <Stack direction="row" spacing={1}>
         <TextField
           required
-          label="Tittel"
-          id="song-title"
+          label="Toneart"
+          id="key"
+          error={inputError === 'key'}
+          helperText={inputError === 'key' ? 'Feil format' : undefined}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            dispatch(setTitle(e.target.value))
+            dispatch(setKey(e.target.value))
           }
-          value={state.title}
+          value={state.key}
         />
-
-        {/* Release date */}
-        <DatePicker
-          label="Utgivelsesdato"
-          value={state.releaseDate}
-          onChange={(newValue) => dispatch(setReleaseDate(newValue))}
-          renderInput={(params) => <TextField {...params} />}
-        />
-
-        {/* Themes */}
-        <Autocomplete
-          multiple
-          id="themes"
-          options={allThemes}
-          getOptionLabel={(theme) => theme.title}
-          value={state.themes}
-          onChange={(_, newValue) => {
-            dispatch(setThemes(newValue));
-          }}
-          noOptionsText="Kunne ikke hente tema."
-          renderInput={(params) => {
-            const themesLimit = 4;
-            const themesCount = state.themes.length;
-            const tooManyChosen = themesCount > themesLimit;
-
-            return (
-              <TextField
-                {...params}
-                variant="outlined"
-                label="Tema"
-                placeholder={themesCount + 1 > themesLimit ? '' : 'Tema'}
-                error={tooManyChosen}
-                helperText={
-                  tooManyChosen
-                    ? 'For mange tema. Du kan maks 4 tema.'
-                    : 'Velg opp til 4 tema.'
-                }
-              />
-            );
-          }}
-        />
-
-        {/* Key, tempo, time */}
-        <Stack direction="row" spacing={1}>
-          <TextField
-            required
-            label="Toneart"
-            id="key"
-            error={inputError === 'key'}
-            helperText={inputError === 'key' ? 'Feil format' : undefined}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              dispatch(setKey(e.target.value))
-            }
-            value={state.key}
-          />
-          <TextField
-            label="Tempo"
-            id="tempo"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              dispatch(setTempo(e.target.value))
-            }
-            value={state.tempo}
-            placeholder="120"
-          />
-          <TextField
-            label="Time"
-            id="time"
-            error={inputError === 'time'}
-            helperText={inputError === 'time' ? 'Feil format' : undefined}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              dispatch(setTime(e.target.value))
-            }
-            value={state.time}
-            placeholder="4/4"
-          />
-        </Stack>
-
-        {/* Optional metadata */}
-
-        {/* Writers */}
-        <ContributorsWithPreview
-          label="Låtskrivere"
-          id="writers"
-          placeholder="Ola, Kari"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            dispatch(setWritersString(e.target.value))
-          }
-          valueString={state.writersString}
-          valueList={state.writersList}
-        />
-
-        {/* Producers */}
-        <ContributorsWithPreview
-          label="Produsent(er)"
-          id="producers"
-          placeholder="Ola, Kari (med-produsent)"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            dispatch(setProducersString(e.target.value))
-          }
-          valueString={state.producersString}
-          valueList={state.producersList}
-          helperText="Skriv rolle i parantes om ønskelig."
-        />
-
-        {/* Contributors */}
-        <ContributorsWithPreview
-          label="Bidragsytere"
-          id="contributors"
-          placeholder="Ola (gitar), Kari (vokal)"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            dispatch(setContributorsString(e.target.value))
-          }
-          valueString={state.contributorsString}
-          valueList={state.contributorsList}
-          helperText="Skriv rolle i parantes om ønskelig."
-        />
-
-        {/* Links */}
-
-        {/* Spotify */}
         <TextField
-          label="Link til Spotify"
-          id="song-spotify-link"
+          label="Tempo"
+          id="tempo"
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            dispatch(setSpotifyLink(e.target.value))
+            dispatch(setTempo(e.target.value))
           }
-          value={state.spotifyLink}
+          value={state.tempo}
+          placeholder="120"
         />
-
-        {/* Apple Music */}
         <TextField
-          label="Link til Apple Music"
-          id="song-apple-music-link"
+          label="Time"
+          id="time"
+          error={inputError === 'time'}
+          helperText={inputError === 'time' ? 'Feil format' : undefined}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            dispatch(setAppleMusicLink(e.target.value))
+            dispatch(setTime(e.target.value))
           }
-          value={state.appleMusicLink}
+          value={state.time}
+          placeholder="4/4"
         />
+      </Stack>
 
+      {/* Optional metadata */}
+
+      {/* Writers */}
+      <ContributorsWithPreview
+        label="Låtskrivere"
+        id="writers"
+        placeholder="Ola, Kari"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          dispatch(setWritersString(e.target.value))
+        }
+        valueString={state.writersString}
+        valueList={state.writersList}
+      />
+
+      {/* Producers */}
+      <ContributorsWithPreview
+        label="Produsent(er)"
+        id="producers"
+        placeholder="Ola, Kari (med-produsent)"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          dispatch(setProducersString(e.target.value))
+        }
+        valueString={state.producersString}
+        valueList={state.producersList}
+        helperText="Skriv rolle i parantes om ønskelig."
+      />
+
+      {/* Contributors */}
+      <ContributorsWithPreview
+        label="Bidragsytere"
+        id="contributors"
+        placeholder="Ola (gitar), Kari (vokal)"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          dispatch(setContributorsString(e.target.value))
+        }
+        valueString={state.contributorsString}
+        valueList={state.contributorsList}
+        helperText="Skriv rolle i parantes om ønskelig."
+      />
+
+      {/* Links */}
+
+      {/* Spotify */}
+      <TextField
+        label="Link til Spotify"
+        id="song-spotify-link"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          dispatch(setSpotifyLink(e.target.value))
+        }
+        value={state.spotifyLink}
+      />
+
+      {/* Apple Music */}
+      <TextField
+        label="Link til Apple Music"
+        id="song-apple-music-link"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          dispatch(setAppleMusicLink(e.target.value))
+        }
+        value={state.appleMusicLink}
+      />
+      {loading ? (
+        <CircularProgress color="inherit" size={20} />
+      ) : (
         <Button type="submit" variant="contained" color="primary">
           Send inn!
         </Button>
-      </SubmitSongFormWrapper>
-    </>
+      )}
+    </SubmitSongFormWrapper>
   );
 };
 
